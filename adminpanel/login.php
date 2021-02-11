3a8d32a5da5ffb5d348512b095c7db6603107911
rhexa <akun.kripitit@gmail.com>
2021-02-11 15:42:37 +0200

    fixed minor issue

--- adminpanel/login.php
+++ adminpanel/login.php
@@ -1,36 +1,33 @@
 <?php
-    include("config.php");
-    session_start();
-    if (isset($_SESSION['login_user'])){
-        header("location: dashboard.php");
-        die();
-    }
-    else {
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
-            // username and password sent from form 
-            
-            $myusername = mysqli_real_escape_string($conn,$_POST['username']);
-            $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
-            
-            $sql = "SELECT id FROM webadmin WHERE username = '$myusername' and password = '$mypassword'";
-            $result = mysqli_query($conn,$sql);
-            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-            $active = $row['active'];
-            
-            $count = mysqli_num_rows($result);
-            
-            // If result matched $myusername and $mypassword, table row must be 1 row
-            
-            if($count == 1) {
-                $_SESSION['login_user'] = $myusername;
-                header("location: dashboard.php");
-                die();
-            }else {
-                $error = "Your Login Name or Password is invalid";
-                echo $_SESSION['login_user'];
-            }
+    $page = "login";
+    include "config.php";
+    include "session.php";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        // username and password sent from form 
+        
+        $myusername = mysqli_real_escape_string($conn,$_POST['username']);
+        $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+        
+        $sql = "SELECT id FROM webadmin WHERE username = '$myusername' and password = '$mypassword'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $active = $row['active'];
+        
+        $count = mysqli_num_rows($result);
+        
+        // If result matched $myusername and $mypassword, table row must be 1 row
+        
+        if($count == 1) {
+            $_SESSION['login_user'] = $myusername;
+            header("location: dashboard.php");
+            die();
+        }else {
+            $error = "Your Login Name or Password is invalid";
+            echo $_SESSION['login_user'];
         }
-   }
+    }
+   
    
 ?>
 <!DOCTYPE html>
@@ -39,16 +36,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <script type="text/javascript">
-    if ( window.history.replaceState ) {
-        window.history.replaceState( null, null, window.location.href );
-    }
-    </script> -->
     <title>Document</title>
 </head>
 <body>
-<?php /* echo htmlspecialchars($_SERVER["PHP_SELF"]); */?>
-    <form action="" method="post">
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <table>
         <tr>
             <td>
--- adminpanel/session.php
+++ adminpanel/session.php
@@ -1,17 +1,26 @@
 <?php
-   include('config.php');
-   session_start();
+    include('config.php');
+    session_start();
+    
+    $user_check = $_SESSION['login_user'];
+    
+    $ses_sql = mysqli_query($conn,"select username from webadmin where username = '$user_check' ");
+    
+    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+    
+    $login_session = $row['username'];
    
-   $user_check = $_SESSION['login_user'];
+    if ($page != "login"){
+        if(!isset($_SESSION['login_user'])){
+            header("location:login.php");
+            die();
+        }
+    }
+    elseif ($page == "login") {
+        if (isset($_SESSION['login_user'])){
+            header("location: dashboard.php");
+            die();
+        }
+    }
    
-   $ses_sql = mysqli_query($conn,"select username from webadmin where username = '$user_check' ");
-   
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-   
-   $login_session = $row['username'];
-   
-   if(!isset($_SESSION['login_user'])){
-      header("location:login.php");
-      die();
-   }
 ?>
