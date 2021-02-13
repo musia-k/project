7dd5077243c40a17ac5555537a30f94d2d5236fd
rhexa <akun.kripitit@gmail.com>
2021-02-13 11:43:00 +0200

    added delete confirmation

--- adminpanel/delete.php
+++ adminpanel/delete.php
@@ -5,13 +5,30 @@ $id = htmlentities($_GET['id']);
 
 $sql = "DELETE FROM cars WHERE id='$id'";
 
-if($conn->query($sql)){
-    header("location: dashboard.php");
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if (isset($_POST['delete'])){
+        if($conn->query($sql)){
+            header("location: dashboard.php");
+        }
+        else {
+            $error = "Record Not Deleted";
+            header("location: dashboard.php?error=$error&id=$id");
+        }
+        $conn-> close();
+        die();
+    }
+    if (isset($_POST['cancel'])){
+        header("location: dashboard.php");
+    }
 }
-else {
-    $error = "Record Not Deleted";
-    header("location: dashboard.php?error=$error&id=$id");
-}
-$conn-> close();
-die();
+
 ?>
+<html>
+You are about to delete a row from the database <br>
+<form action="" method="post">
+    <table>
+        <td><input type="submit" name="delete" value="Delete"></td>
+        <td><input type="submit" name="cancel" value="Cancel"></td>
+    </table>
+</form>
+</html>
