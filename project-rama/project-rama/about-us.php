e975a4b5284ee60420ae907168ae16a05f573461
RavinduHerath <77496010+RavinduHerath@users.noreply.github.com>
2021-02-15 19:46:53 +0530

    Add files via upload

---
+++ project-rama/project-rama/about-us.php
@@ -0,0 +1,35 @@
+<?php
+$page = "about-us";
+include "header.php"; 
+?> 
+
+<main>
+    <div class="container-content">
+        <div class="row">
+            <div class="d-none d-sm-block col col-sm column">
+                <img class="img_word" src="src\images\ABOUT OUR COMPANY ...png" alt=""" width="35%" style="margin-left: 40px;">
+            </div>
+            <div class="d-block d-sm-none col-12 text-center">
+                <h1>ABOUT OUR COMPANY</h1>
+            </div>
+            <div class="col column mx-auto">
+                <img src="src\images\About_Us.jpg" alt="" width="100%">
+            </div>
+        </div>
+        <div class="row mx-auto">
+            <div class="d-none d-sm-block col">
+                <div class="image2">
+                    <img src="src\images\logo-nav-header.png" alt="" width="100%"">
+                </DIV>
+            </div>
+            <div class="col container-text mx-auto">
+                <p style="margin-left: 30px;">RMR company was established 2 years ago. Through the years the company gained a well-established reputation for its commitment to offer quality, timely and unparalleled customer service. Our goal is to provide the ability to rent the
+                    car for everyone. <br> <br> Since our clientele is diversified, we offer a wide selection of new models starting from small city cars, medium sized, automatic and luxury cars able to satisfy or give mobile solution at any of your
+                    professional or private needs. All car models we provide, are new and in excellent condition.</p>
+            </div>
+        </div>
+        
+    </div>
+</main>
+
+<?php include "footer.php"; ?> 
