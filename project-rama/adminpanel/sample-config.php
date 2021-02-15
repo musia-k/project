2885540a99781e5635b40b502eca63db3f9c576d
RavinduHerath <77496010+RavinduHerath@users.noreply.github.com>
2021-02-15 22:15:57 +0530

    Add files via upload

---
+++ project-rama/adminpanel/sample-config.php
@@ -0,0 +1,12 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="root";
+$dbname ="inventory";
+// creating connection 
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) 
+    die("Connection failed: ".$conn->connect_error);
+?>
---
+++ project-rama/home.php
@@ -0,0 +1,27 @@
+<?php
+$page = "home";
+include "header.php"; 
+?> 
+
+<!-- Create the content here -->
+<main>
+
+    <div class="container">
+        <div class="row md-2">
+            <div class="col-md-6"> 
+                <div class="row md-2">
+                    <div class="col-md-3">
+                        <img src="src/images/car_main_page (1).png" alt="car">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6"><h1>Available <br>Renting for Everyone ..</h1></div>
+        </div>
+    </div>
+    <div class="contact-right">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d254499.81439169744!2d24.7385084!3d60.1098678!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bc796210691%3A0xcd4ebd843be2f763!2sHelsinki%2C%20Finland!5e0!3m2!1sen!2slk!4v1613405883150!5m2!1sen!2slk" width="400" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+</main>
+<!-- end of content -->
+
+<?php include "footer.php"; ?> 
