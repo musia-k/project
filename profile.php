7aaca0b36075035db108680ecd48751bc2a7a7d0
rhexa <akun.kripitit@gmail.com>
2021-03-01 21:22:27 +0200

    sidebar 2

--- profile.php
+++ profile.php
@@ -7,11 +7,27 @@ include 'header.php';
     <div class="container-content mx-auto" id="container-content">
         <!-- sidebar -->
         <div class="sidebar-wrapper">
-            <ul class="sidebar-nav mx-auto">
-                <li><a href="#" id="sidebar-toggle"><i class="fas fa-bars"></i></a></li>
-                <li><a href="#"><i class="fas fa-book-open"></i></a></li>
-                <li><a href="#"><i class="fas fa-address-card"></i></a></li>
-            </ul>
+            <div class="sidebar-nav mx-auto">
+                <div class="sidebar-link">
+                    <a href="#" id="sidebar-toggle">
+                        <div class="sidebar-icon"><i class="fas fa-bars"></i></div>
+                    </a>
+                
+                </div>
+                <div class="sidebar-link">
+                    <a href="#">
+                        <div class="sidebar-icon"><i class="fas fa-book-open"></i></div>
+                        <span class="sidebar-label">Reservation</span>
+                    </a>
+                
+                </div>
+                <div class="sidebar-link">
+                    <a href="#">
+                        <div class="sidebar-icon"><i class="fas fa-address-card"></i></div>
+                        <span class="sidebar-label">Profile</span>
+                    </a>
+                </div>
+            </div>
         </div>
 
         <!-- content -->
