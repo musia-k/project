941c1c69008b5ffcf9003a7e7510176fece64b43
rhexa <akun.kripitit@gmail.com>
2021-03-02 15:24:21 +0200

    created sidebar.php

--- profile.php
+++ profile.php
@@ -5,30 +5,8 @@ include 'header.php';
 ?>
 <main>
     <div class="container-content mx-auto" id="container-content">
-        <!-- sidebar -->
-        <div class="sidebar-wrapper">
-            <div class="sidebar-nav mx-auto">
-                <div class="sidebar-link">
-                    <a href="#" id="sidebar-toggle">
-                        <div class="sidebar-icon"><i class="fas fa-bars"></i></div>
-                    </a>
-                
-                </div>
-                <div class="sidebar-link">
-                    <a href="#">
-                        <div class="sidebar-icon"><i class="fas fa-book-open"></i></div>
-                        <span class="sidebar-label">Reservation</span>
-                    </a>
-                
-                </div>
-                <div class="sidebar-link">
-                    <a href="#">
-                        <div class="sidebar-icon"><i class="fas fa-address-card"></i></div>
-                        <span class="sidebar-label">Profile</span>
-                    </a>
-                </div>
-            </div>
-        </div>
+        
+        <?php include 'sidebar.php'; ?>
 
         <!-- content -->
         <div class="content-wrapper">
---
+++ sidebar.php
@@ -0,0 +1,25 @@
+<!-- sidebar -->
+<div class="sidebar-wrapper">
+    <div class="sidebar-nav mx-auto">
+        <div class="sidebar-link">
+            <a href="#" id="sidebar-toggle">
+                <div class="sidebar-icon"><i class="fas fa-bars"></i></div>
+            </a>
+        
+        </div>
+        <div class="sidebar-link">
+            <a href="#">
+                <div class="sidebar-icon"><i class="fas fa-book-open"></i></div>
+                <span class="sidebar-label">Reservation</span>
+            </a>
+        
+        </div>
+        <div class="sidebar-link">
+            <a href="#">
+                <div class="sidebar-icon"><i class="fas fa-address-card"></i></div>
+                <span class="sidebar-label">Profile</span>
+            </a>
+        </div>
+    </div>
+</div>
+<!-- end sidebar -->
