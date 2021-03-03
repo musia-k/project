d7f6c6690ba09cb680166fbfe6a3db11b2759b68
rhexa <akun.kripitit@gmail.com>
2021-03-03 20:27:43 +0200

    navbar gallery/gallery.php

---
+++ gallery/navbar.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="custom-header">
+        <a href="home.php" class="logo"><img src="logo-nav-header.png" alt="rmr" class="header-image"></a>
+        <a href="#" id="toggle-nav">
+            <i class="fas fa-bars"></i>
+        </a>
+        <nav class="custom-navbar">
+            <div class="link"><a href="home.php">HOME</a></div>
+            <div class="link"><a href="gallery.php" class="active">GALLERY</a></div>
+            <div class="link"><a href="about-us.php">ABOUT US</a></div>
+            <div class="link"><a href="faq.php">FAQ</a></div>
+            <div class="link"><a href="rules.php">RULES</a></div>
+        </nav>
+    </div>
+</header>
