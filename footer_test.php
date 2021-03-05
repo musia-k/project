aff8349b05299b2551b8fa57a394228b4d15e2e3
musia-k <77528316+musia-k@users.noreply.github.com>
2021-03-05 10:07:52 +0200

    New version of footer (no bootstrap)
    New version of footer (no bootstrap)

---
+++ footer_test.php
@@ -0,0 +1,39 @@
+<!-- footer -->
+<footer class="custom-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col_1">
+                        <div class="text-footer">
+                            <h3>Car Rental in Helsinki</h3>
+                            <a href="gallery.html">Gallery</a> <br>
+                            <a href="gallery.html">Special offers</a> <br>
+                            <a href="gallery.html">Business renters</a> <br>
+                            <a href="gallery.html">Vehicles</a>
+                        </div>
+                    </div>
+                    <div class="col_2">
+                        <div class="text-footer">
+                            <h3>Customer Service</h3>
+                            <a href="about-us.html">Contact us</a> <br>
+                            <a href="faq.html">Frequently Asked Questions</a> 
+                        </div>
+                    </div>
+                    <div class="col_3">
+
+                    </div>
+                    <div class="col_4">
+                        <div class="text-footer">
+                            <p>© 2021 <br>
+                                RMR Car Rental.
+                                All rights reserved. Please read our Privacy Policy.
+                                Powered by Wheels Car Rental System
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end of footer -->
+    </div> <!-- end container-main -->
+  </body>
+</html>
