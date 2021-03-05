804d955727d6613fbe5658709ce5668f18dbfaee
musia-k <77528316+musia-k@users.noreply.github.com>
2021-03-05 11:54:09 +0200

    Update home.php

--- home.php
+++ home.php
@@ -17,9 +17,31 @@ include "header.php";
             </div>
             <div class="col-md-6"><h1>Available <br>Renting for Everyone ..</h1></div>
         </div>
-    </div>
-
+    
 </main>
+<div class="container">
+<div class="row md-2" style="background-color: white; max-height:400px;">
+            <div class="col-md-6"> 
+                <div class="row md-2">
+                    <div class="col-md-8" style="margin-left:70px; margin-top: 35px;">
+                        <h2><strong> Contact us </strong></h2>
+                        <p><strong>Local number:</strong> 044 225 03 86<br>
+                        <strong>International number:</strong> +44 161 602 5561  <br>  <br>
+                          
+                        <strong>You can also write to us at: </strong>reservations@rmr_cars.com </p>
+                        <a href="https://www.facebook.com/renthelsinki/"><img src="https://cdn3.iconfinder.com/data/icons/facebook-ui-flat/48/Facebook_UI-03-512.png" width="50" height="50" alt="facebook"></a>
+                        <a href="https://instagram.com/rmr_renting?igshid=sxobv4jw32ni"><img src="https://image.flaticon.com/icons/png/512/87/87390.png" width="30" height="30" alt="insta"></a>
+                        <a href="https://mail.google.com/mail/"><img src="https://cdn.iconscout.com/icon/free/png-256/gmail-32-761667.png" width="30" height="30" alt="mail" style="margin-left:10px;"></a>
+                        
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4" style="margin-left:100px; margin-top:35px;margin-bottom:35px;" >
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d254499.81439169744!2d24.7385084!3d60.1098678!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bc796210691%3A0xcd4ebd843be2f763!2sHelsinki%2C%20Finland!5e0!3m2!1sen!2slk!4v1613405883150!5m2!1sen!2slk" width="400" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+</div>
+    </main>
 <!-- end of content -->
 
 <?php include "footer.php"; ?> 
