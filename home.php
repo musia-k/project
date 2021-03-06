378bda2c3a8fea5da5fa7265e28b1c4c5d4c8444
musia-k <77528316+musia-k@users.noreply.github.com>
2021-03-06 11:37:45 +0200

    Update home.php

--- home.php
+++ home.php
@@ -23,7 +23,7 @@ include "header.php";
 <div class="row md-2" style="">
             <div class="col-md-6"> 
                 <div class="row md-2" style="max-height:400px;max-width: 1100px;">
-                    <div class="col-md-8" style="max-height:500px;">
+                    <div class="col-md-6" style="max-height:500px;">
                         <h2><strong> Contact us </strong></h2>
                         <p><strong>Local number:</strong> 044 225 03 86<br>
                         <strong>International number:</strong> +44 161 602 5561  <br>  <br>
@@ -37,11 +37,12 @@ include "header.php";
                     </div>
                 </div>
             </div>
-            <div class="col-md-2" style="max-width: 600px; padding-left: 70px">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d254499.81439169744!2d24.7385084!3d60.1098678!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bc796210691%3A0xcd4ebd843be2f763!2sHelsinki%2C%20Finland!5e0!3m2!1sen!2slk!4v1613405883150!5m2!1sen!2slk" wight="400" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <div class="col-md-4">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d254499.81439169744!2d24.7385084!3d60.1098678!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bc796210691%3A0xcd4ebd843be2f763!2sHelsinki%2C%20Finland!5e0!3m2!1sen!2slk!4v1613405883150!5m2!1sen!2slk"  height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
 </div>
     </main>
 <!-- end of content -->
 
 <?php include "footer.php"; ?> 
+
